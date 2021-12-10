6f912db94b9276567235449739f7390178177121
Jonas Frey <dev@jonasfrey.de>
2021-12-10 14:12:01 +0100

    Initial Commit

---
+++ JFUtils/JFUtilsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JFUtilsApp.swift
+//  JFUtils
+//
+//  Created by Jonas Frey on 10.12.21.
+//
+
+import SwiftUI
+
+@main
+struct JFUtilsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
